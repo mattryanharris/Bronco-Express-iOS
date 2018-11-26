b41d4d86e8a2d710f79974797f9db0f189165eed
mattryanharris <matthew@biofare.org>
2018-11-26 12:46:55 -0800

    Start of making this app

---
+++ Bronco Express/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bronco Express
+//
+//  Created by Matthew Harris on 11/26/18.
+//  Copyright © 2018 Matthew Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    let url = "https://broncoshuttle.com/Route/3164/Stop/1592066/Arrivals?customerID=21"
+    
+    
+    
+
+
+}
+
